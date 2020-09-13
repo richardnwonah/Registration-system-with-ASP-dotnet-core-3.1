e3dea3c283d9680d39db253f01d7d7dd581d3f08
Richard Nwonah <nwonahr@gmail.com>
2020-09-13 22:20:01 +0100

    aded project

---
+++ src/RegistrationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RegistrationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
